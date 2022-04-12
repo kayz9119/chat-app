ee5e14281aae562bc7123ed8de6370fe0aeb6e37
kayz9119 <solanahtarigibiyim@gmail.com>
2022-04-12 11:01:12 +0300

    update

--- index.js
+++ index.js
@@ -9,12 +9,28 @@ const server = app.listen(process.env.PORT||3000, () => {
 app.use(express.static('public'))
 const io = socket(server)
 io.on('connection', (socket) => {
-    console.log(socket.id)
+    
+    io.socketsJoin('chat1');
+
     socket.on('chat', data =>  {
-        io.sockets.emit('chat', data)
-    })
+        if(data.message !== ''){
+            io.sockets.emit('chat', data);
+        }
+    });
 
     socket.on('typing', data => {
         socket.broadcast.emit('typing', data)
     })
 })
+async function fetching(){
+    sockets = await io.fetchSockets();
+    return sockets;
+}
+async function fetchByRoom(roomName){
+    sockets = await io.in(roomName).fetchSockets();
+    return sockets;
+}
+async function fetchBySocketID(socketID){
+    sockets = await io.in(socketID).fetchSockets();
+    return sockets;
+}
--- public/chat.js
+++ public/chat.js
@@ -1,17 +1,41 @@
-const socket = io.connect('localhost:3000')
+const socket = io.connect('192.168.1.100:3000')
 
 const sender = document.getElementById('sender')
 const message = document.getElementById('message')
 const submitBtn = document.getElementById('submitBtn')
 const output = document.getElementById('output')
 const feedback = document.getElementById('feedback')
+const chat_win = document.getElementById('chat-window')
+const autoscroll = document.getElementById('autoscroll')
 
 submitBtn.addEventListener('click', () => {
+
     socket.emit('chat', {
         message: message.value,
         sender: sender.value,
-    })
-})
+    });
+    
+});
+
+socket.on('connect', function(client){
+     name = prompt('name');
+     socket.emit('chat', {
+         message: name + ' bağlandı.',
+         sender: 'admin'
+     });
+     sender.value = name;
+     if(name){
+        sayhi = confirm('Selam vermeye ne dersin?');
+        
+        if(sayhi){
+            message.value = "Merhaba!";
+            socket.emit('chat', {
+                message: message.value,
+                sender: sender.value,
+            });
+        }
+     }
+});
 
 socket.on('chat', data => {
     feedback.innerHTML = '';
@@ -27,4 +51,17 @@ message.addEventListener('keypress', () => {
 
 socket.on('typing', data => {
     feedback.innerHTML = `<p>${data} is typing...</p>`
-})
+});
+
+
+autoscroll.addEventListener('change', function(e) {
+    if(autoscroll.checked == true){
+        pageScroll();
+    }else{
+        console.log(autoscroll.checked)
+    }
+});
+function pageScroll() {
+    chat_win.scrollBy(0, 5);
+    scrolldelay = setTimeout(pageScroll, 10);
+}
